ec3c83749834be4c0bbeaf51a5c422e4561b21d4
christianhoward <christian.john.howard@gmail.com>
2017-10-02 10:45:33 -0400

    Users can see survey results

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 const keys = require('./config/keys');
 
@@ -26,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 // used to handle the build files on production side
 if (process.env.NODE_ENV === 'production') {
